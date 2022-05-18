0433f16eb764cf86d7d097fed75de0f7861a1608
andreybobrovnik <ndesign.by@gmail.com>
2022-05-18 15:52:33 +0300

    deps

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,27 @@ import Footer from "./footer";
 import Header from "./header";
 import "../styles/boot.scss";
 import "../styles/layout.scss";
+import CookieConsent from "react-cookie-consent";
+import { useLocation } from "@reach/router" // this helps tracking the location
+import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies'
 
 function Layout({ children, classmain = "page" }) {
+  const location = useLocation()
   return (
     <React.Fragment>
       <Header />
       <main className={"overflow main " + classmain}>{children}</main>
       <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        declineButtonText="Decline"
+        cookieName="gatsby-gdpr-google-tagmanager"
+        onAccept={() => initializeAndTrack(location)}
+      >
+        In order to deliver superior & personalized user experience we use
+        cookies.
+      </CookieConsent>
     </React.Fragment>
   );
 }
