e04dba57c487df5b8f72e03b2f97eb9ff9b9e2b4
Andrew Bro <ndesign.by@gmail.com>
2023-02-27 10:50:39 +0300

    lateset disallow

--- gatsby-config.js
+++ gatsby-config.js
@@ -191,7 +191,7 @@ module.exports = {
   {
     resolve: `gatsby-plugin-sitemap`,
     options: {
-      excludes: ["/404"],
+      excludes: ["/404", "/latest/**"],
     },
   },
   "gatsby-plugin-sass",
