2452cbd8acc25f4c87d3aa36621461be1b5b4caf
AdBro <ndesign.by@gmail.com>
2023-04-14 10:52:33 +0300

    go

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,127 +1,127 @@
-const _ = require("lodash")
 const fetch = require('node-fetch');
 const redirects = require("./redirects.json");
 
 exports.createPages = async function ({ actions, graphql }) {
-  const { createRedirect } = actions
+  const { createRedirect } = actions;
 
-    const { data } = await graphql(`{
-  posts: allWpPost(filter: {status: {eq: "publish"}}) {
-    edges {
-      node {
-        slug
-        id
-        guid
-        categories {
-          nodes {
-            name
-            slug
+  const { data } = await graphql(`{
+    posts: allWpPost(filter: {status: {eq: "publish"}}) {
+      edges {
+        node {
+          slug
+          id
+          guid
+          categories {
+            nodes {
+              name
+              slug
+            }
           }
+          uri
+          content
         }
-        uri
-        content
+      }
+      totalCount
+    }
+    catGroup: allWpCategory {
+      group(field: { uri: SELECT }) {
+        fieldValue
+        sum(field: { count: SELECT })
       }
     }
-    totalCount
-  }
-  catGroup: allWpCategory {
-    group(field: {uri: SELECT}) {
-      fieldValue
-      sum(field: {count: SELECT})
+  }`);
+
+  // Create posts
+  data.posts.edges.forEach(edge => {
+    const postUri = edge.node.uri;
+    const postSlug = edge.node.slug;
+    const id = edge.node.id;
+    actions.createPage({
+      path: postUri,
+      id: id,
+      component: require.resolve(`./src/templates/blog-post.js`),
+      context: { slug: postSlug },
+    });
+  });
+
+  // Make category pages
+  data.catGroup.group.forEach(cat => {
+    const catSlug = cat.fieldValue;
+    const catName = cat.fieldValue;
+    const posts = cat.sum;
+    const postsPerPage = 12;
+    const numPages = Math.ceil(posts / postsPerPage);
+
+    for (let i = 0; i < numPages; i++) {
+      actions.createPage({
+        path: i === 0 ? `${catSlug}` : `${catSlug}/${i + 1}`,
+        component: require.resolve(`./src/templates/category-template.js`),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1,
+          cat: catName,
+          uri: catSlug
+        },
+      });
     }
+  });
+
+  // Make latest page
+  const allPosts = data.posts.totalCount;
+  const allPostsPerPage = 12;
+  const allNumPages = Math.ceil(allPosts / allPostsPerPage);
+
+  for (let i = 0; i < allNumPages; i++) {
+    actions.createPage({
+      path: i === 0 ? `/latest` : `/latest/${i + 1}`,
+      component: require.resolve(`./src/templates/blog-list.js`),
+      context: {
+        limit: allPostsPerPage,
+        skip: i * allPostsPerPage,
+        allNumPages,
+        currentPage: i + 1,
+      },
+    });
   }
-}`)
 
-    // Create posts
-    data.posts.edges.forEach(edge => {
-      const postUri = edge.node.uri
-      const postSlug = edge.node.slug
-      const id = edge.node.id
-      actions.createPage({
-        path: postUri,
-        id: id,
-        component: require.resolve(`./src/templates/blog-post.js`),
-        context: { slug: postSlug },
-      })
+  // Make homepage
+  actions.createPage({
+    path: '/',
+    id: 'homepage',
+    component: require.resolve(`./src/templates/homepage.js`),
+    context: { slug: '/' },
+  });
+
+  actions.createPage({
+    path: '/privacy',
+    id: 'privacy',
+    component: require.resolve(`./src/templates/privacy.js`),
+    context: { slug: '/privacy' },
+  });
+
+  actions.createPage({
+    path: '/terms',
+    id: 'terms',
+    component: require.resolve(`./src/templates/terms.js`),
+    context: { slug: '/terms' },
+  });
+
+  actions.createPage({
+    path: '/thank-you',
+    id: 'thank-you',
+    component: require.resolve(`./src/templates/thank-you.js`),
+    context: { slug: '/thank-you' },
+  });
+
+  redirects.forEach(redirect => 
+    createRedirect({
+      fromPath: redirect.fromPath,
+      toPath: redirect.toPath,
+      statusCode: redirect.status,
+      redirectInBrowser: true,
+      isPermanent: true,
     })
-
-    // Make category pages
-      data.catGroup.group.forEach(cat => {
-        const catSlug = cat.fieldValue
-        const catName = cat.fieldValue
-
-        const posts = cat.sum
-        const postsPerPage = 12
-        const numPages = Math.ceil(posts / postsPerPage)
-        Array.from({ length: numPages > 1 ? numPages : "1" }).forEach((_, i) => {
-          actions.createPage({
-            path: i === 0 ? `${catSlug}` : `${catSlug}/${i + 1}`,
-            component: require.resolve(`./src/templates/category-template.js`),
-            context: {
-              limit: postsPerPage,
-              skip: i * postsPerPage,
-              numPages,
-              currentPage: i + 1,
-              cat: catName,
-              uri: catSlug
-            },
-          })
-        })
-      })
-
-    // Make latest page
-      const allposts = data.posts.totalCount
-      const allpostsPerPage = 12
-      const allnumPages = Math.ceil(allposts / allpostsPerPage)
-      Array.from({ length: allnumPages }).forEach((_, i) => {
-        actions.createPage({
-          path: i === 0 ? `/latest` : `/latest/${i + 1}`,
-          component: require.resolve(`./src/templates/blog-list.js`),
-          context: {
-            limit: allpostsPerPage,
-            skip: i * allpostsPerPage,
-            allnumPages,
-            currentPage: i + 1,
-          },
-        })
-      })
-
-      // Make homepage
-      actions.createPage({
-        path: '/',
-        id: 'homepage',
-        component: require.resolve(`./src/templates/homepage.js`),
-        context: { slug: '/' },
-      })
-
-      actions.createPage({
-        path: '/privacy',
-        id: 'privacy',
-        component: require.resolve(`./src/templates/privacy.js`),
-        context: { slug: '/privacy' },
-      })
-
-      actions.createPage({
-        path: '/terms',
-        id: 'terms',
-        component: require.resolve(`./src/templates/terms.js`),
-        context: { slug: '/terms' },
-      })
-
-      actions.createPage({
-        path: '/thank-you',
-        id: 'thank-you',
-        component: require.resolve(`./src/templates/thank-you.js`),
-        context: { slug: '/thank-you' },
-      })
-
-      redirects.forEach(redirect => 
-        createRedirect({
-          fromPath: redirect.fromPath,
-          toPath: redirect.toPath,
-          statusCode: redirect.status,
-          redirectInBrowser: true,
-          isPermanent: true,
-        })
-      )
+  )
 }
--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -11,9 +11,9 @@ import Ads from "../components/ads";
 // Components
 import { graphql } from "gatsby"
 const BlogList = ({ pageContext, data:{postdata, hotImage} }) => {
-  const { currentPage, allnumPages } = pageContext
+  const { currentPage, allNumPages } = pageContext
   const isFirst = currentPage === 1
-  const isLast = currentPage === allnumPages
+  const isLast = currentPage === allNumPages
   const prevPage = currentPage - 1 === 1 ? '/latest/' : '/latest/' + (currentPage - 1).toString()
   const nextPage = '/latest/' + (currentPage + 1).toString()
   return (
@@ -38,7 +38,7 @@ const BlogList = ({ pageContext, data:{postdata, hotImage} }) => {
                 </svg>
               </Link>
             )}
-            {Array.from({ length: allnumPages > currentPage + 10 ? currentPage + 10 : allnumPages - currentPage }, (_, i) => (
+            {Array.from({ length: allNumPages > currentPage + 10 ? currentPage + 10 : allNumPages - currentPage }, (_, i) => (
               <li
                 key={`pagination-number${currentPage + 1}`}
                 className={`nota${(i === currentPage - 1 && ' activeli') || (i > currentPage + 2 && ' disactiveli') || (i < currentPage - 2 && ' disactiveli') || ''}`}
@@ -50,7 +50,7 @@ const BlogList = ({ pageContext, data:{postdata, hotImage} }) => {
                 </Link>
               </li>
             ))}
-            {!isLast && allnumPages > 1  && (
+            {!isLast && allNumPages > 1  && (
               <Link to={nextPage} rel="next" className="next">
                 <svg width="54" height="26" viewBox="0 0 54 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M45.8585 15L0 15V11L45.8581 11L37.6484 3.4745L40.3513 0.525879L51.7777 11H52V11.2038L53.9597 13.0002L52 14.7966V15H51.7781L40.3513 25.4745L37.6484 22.5259L45.8585 15Z" fill="white"/>
@@ -60,7 +60,7 @@ const BlogList = ({ pageContext, data:{postdata, hotImage} }) => {
             </ul>
             <div style={{margin:'30px 0'}}>
               <svg style={{width:'100%'}} width="803" height="18" viewBox="0 0 803 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 1.37158L26.0313 16.6287L51.0625 1.37158L76.0938 16.6287L101.125 1.37158L126.156 16.6287L151.188 1.37158L176.219 16.6287L201.25 1.37158L226.281 16.6287L251.313 1.37158L276.344 16.6287L301.375 1.37158L326.406 16.6287L351.438 1.37158L376.469 16.6287L401.5 1.37158L426.531 16.6287L451.563 1.37158L476.594 16.6287L501.625 1.37158L526.656 16.6287L551.688 1.37158L576.719 16.6287L601.75 1.37158L626.781 16.6287L651.813 1.37158L676.844 16.6287L701.875 1.37158L726.906 16.6287L751.938 1.37158L776.969 16.6287L802 1.37158" stroke="#E7F0F8" stroke-width="2"/>
+                <path d="M1 1.37158L26.0313 16.6287L51.0625 1.37158L76.0938 16.6287L101.125 1.37158L126.156 16.6287L151.188 1.37158L176.219 16.6287L201.25 1.37158L226.281 16.6287L251.313 1.37158L276.344 16.6287L301.375 1.37158L326.406 16.6287L351.438 1.37158L376.469 16.6287L401.5 1.37158L426.531 16.6287L451.563 1.37158L476.594 16.6287L501.625 1.37158L526.656 16.6287L551.688 1.37158L576.719 16.6287L601.75 1.37158L626.781 16.6287L651.813 1.37158L676.844 16.6287L701.875 1.37158L726.906 16.6287L751.938 1.37158L776.969 16.6287L802 1.37158" stroke="#E7F0F8" strokeWidth="2"/>
               </svg>
             </div>
             <StaticImage
