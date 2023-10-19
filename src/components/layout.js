cac9a51931e3d94ab6765ee5797a57ec70fd4f5e
Andrew Bro <ndesign.by@gmail.com>
2023-10-19 17:03:42 +0200

    cookie

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,10 @@ import Footer from "./footer";
 import Header from "./header";
 import "../styles/boot.scss";
 import "../styles/layout.scss";
+import CookieConsent from "react-cookie-consent";
 import { useLocation } from "@reach/router" // this helps tracking the location
 import CustomCursor from "./custom-cursor"
+import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies'
 
 function Layout({ children, classmain = "page", title="Udonis" }) {
   const location = useLocation()
@@ -53,6 +55,16 @@ function Layout({ children, classmain = "page", title="Udonis" }) {
         <Header title={title}/>
         <main className="overflow main">{children}</main>
         <Footer />
+        <CookieConsent
+          location="bottom"
+          buttonText="Accept"
+          declineButtonText="Decline"
+          cookieName="gatsby-gdpr-google-tagmanager"
+          onAccept={() => initializeAndTrack(location)}
+        >
+          In order to deliver superior & personalized user experience we use
+          cookies.
+        </CookieConsent>
       </div>
       <CustomCursor isDisplay={isDisplay} position={cursorPosition} />
     </React.Fragment>
