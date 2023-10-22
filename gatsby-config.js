3d94e9f4c5b10ce5c624a1d67978e9866d491d3f
Andrew Bro <ndesign.by@gmail.com>
2023-10-22 12:07:50 +0200

    go

--- gatsby-config.js
+++ gatsby-config.js
@@ -217,6 +217,11 @@ module.exports = {
   {
     resolve: `gatsby-plugin-gdpr-cookies`,
     options: {
+      googleAnalytics: {
+        trackingId: 'G-J3XN01S71Y',
+        cookieName: 'gatsby-gdpr-google-analytics',
+        anonymize: true,
+      },
       googleTagManager: {
         trackingId: 'GTM-N9VPP89F', // leave empty if you want to disable the tracker
         cookieName: 'gatsby-gdpr-google-tagmanager', // default
