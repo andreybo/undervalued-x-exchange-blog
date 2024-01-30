664dc42e4b0b567bb420f5945eb6b01b1d2e17f7
Andrew Bro <ndesign.by@gmail.com>
2024-01-30 23:57:00 +0100

    promo

--- src/components/promo.js
+++ src/components/promo.js
@@ -3,8 +3,8 @@ import * as React from "react";
 export default function Ads() {
 
   return(
-      <>
+      <a href="https://www.udonis.co">
         <img src="https://www.udonis.co/og/homepage.jpg" alt="udonis-ads-1" width="100%" height="auto" />
-      </>
+      </a>
   )
 }
