c636f385287f90ae7cac47d685a4eadcfd8fb0b6
Andrew Bro <ndesign.by@gmail.com>
2024-02-28 13:48:21 +0100

    test browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,12 @@
 
 // You can delete this file if you're not using it
 //import 'lazysizes'
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+    if (prevLocation !== null) {
+      const iframes = document.querySelectorAll('iframe');
+      iframes.forEach(iframe => {
+        iframe.src = iframe.src;
+      });
+    }
+  };
+  
